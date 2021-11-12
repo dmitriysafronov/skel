fec3b30db9b37c7fd7a92da314a34c78eedde8d6
Dmitriy Safronov <zimniy@cyberbrain.pw>
2021-11-12 14:38:39 +0300

    ordering

--- .profile.d/0000-default.sh
+++ .profile.d/0000-default.sh
@@ -11,11 +11,3 @@ fi
 if [ -d "$HOME/.local/bin" ] ; then
     PATH="$HOME/.local/bin:$PATH"
 fi
-
-# if running bash
-if [ -n "$BASH_VERSION" ]; then
-    # include .bashrc if it exists
-    if [ -f "$HOME/.bashrc" ]; then
-        . "$HOME/.bashrc"
-    fi
-fi
